fdbe068224190d2ded548285cebd7b85aa1ff5f3
Jon Gregory Rothlander <j.rothlander@att.net>
2021-12-06 08:26:25 -0600

    Add zero add test.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,10 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var add = new Add();
-            add.AddTest();
-            add.TruncationTest_Failure();
-            add.TruncationTest_Success();          
+            //var add = new Add();
+            //add.AddTest();
+            //add.TruncationTest_Failure();
+            //add.TruncationTest_Success();          
+
+            var zadd = new ZeroAdd();
+            zadd.Test();
 
             Console.ReadKey();
         }
